79a651173a4cc3ee06924272cd8f8850e1a68bce
Martin Bulan <martin.bulan@gmail.com>
2023-01-27 14:08:49 +0100

    Blockchain layout update

--- src/static/script.js
+++ src/static/script.js
@@ -1,36 +1,36 @@
 $(document).ready(function(){
     $("#complexity").click(function(){
-      $("#complex_value").load('/complexity');
+      $("#api_response").load('/complexity');
     });
   });
 
   $(document).ready(function(){
     $("#mine_new_block").click(function(){
-      $("#new_block_alert").load('/mine_block');
+      $("#api_response").load('/mine_block');
     });
   });
 
   $(document).ready(function(){
     $("#get_nodes").click(function(){
-      $("#get_nodes_reponse").load('/get_nodes');
+      $("#api_response").load('/get_nodes');
     });
   });
 
   $(document).ready(function(){
     $("#replace_chain").click(function(){
-      $("#replace_chain_reponse").load('/replace_chain');
+      $("#api_response").load('/replace_chain');
     });
   });
 
   $(document).ready(function(){
     $("#set_complexity").click(function(){
-      $("#new_complex_value").load('/complexity/' + $("#complexity_input").val());
+      $("#api_response").load('/complexity/' + $("#complexity_input").val());
     });
   });
 
   $(document).ready(function(){
     $("#validate_chain").click(function(){
-      $("#validate_chain_reponse").load('/validate');
+      $("#api_response").load('/validate');
     });
   });
 
@@ -41,7 +41,7 @@ $(document).ready(function(){
             url: '/add_transaction',
             data: JSON.stringify ({sender: $("#sender").val(), receiver:$("#receiver").val(), amount: $("#amount").val()}),
             success: function(data) { 
-              $("#transaction_alert").text('Transaction will be added to the next block: ' + 
+              $("#api_response").text('Transaction will be added to the next block: ' + 
               JSON.stringify ({
                 sender: $("#sender").val(), 
                 receiver:$("#receiver").val(), 
