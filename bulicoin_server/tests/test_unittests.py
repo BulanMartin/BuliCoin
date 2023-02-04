f53259e53a026eb32fdb38a9a77a13fa4bcc4107
Martin Bulan <martin.bulan@gmail.com>
2023-02-04 14:37:22 +0100

    Add 2 unittests

--- bulicoin_server/tests/test_unittests.py
+++ bulicoin_server/tests/test_unittests.py
@@ -17,3 +17,30 @@ def test_init():
     assert len(test_chain.chain) == 1
     assert test_chain.leading_zeros == 4
 
+# Check that previous block is returned
+def test_get_prev_block():
+    test_chain = Blockchain()
+
+    prev_block = test_chain.get_prev_block()
+
+    assert isinstance(prev_block, OrderedDict)
+
+    assert prev_block['current_complexity'] == 4
+    assert prev_block['nonce'] == 1
+    assert prev_block['index'] == 1
+    assert isinstance(prev_block['timestamp'], str)
+    assert len(prev_block['timestamp']) == 26
+
+# Test block mining functionality
+def test_mine_block():
+    test_chain = Blockchain()
+
+    prev_block = test_chain.get_prev_block()
+    nonce = test_chain.proof_of_work(prev_block)
+    prev_hash = test_chain.hash(prev_block, nonce)
+    block = test_chain.create_block(nonce, prev_hash)
+
+    assert block['index']== 2
+    assert isinstance(block['timestamp'], str)
+    assert len(block['timestamp']) == 26
+    assert block['current_complexity'] == 4
